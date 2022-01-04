31ecb2bd120c3b01e1a2845393dd1937ca5f242e
yusra <60106667+yusra-bit@users.noreply.github.com>
2022-01-04 09:13:13 +0300

    uploaded

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,71 @@
+  //Detecting Button Press
+
+var numberOfDrumButtons =document.querySelectorAll(".drum").length;
+ 
+for(var i = 0; i<numberOfDrumButtons; i++){
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+      var buttonInnerHTML = this.innerHTML;
+
+      makeSound(buttonInnerHTML);
+      buttonAnimation(buttonInnerHTML);
+});
+
+}
+
+  //Detecting Keyboard Press
+
+document.addEventListener("keypress", function(event){
+    
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+
+    switch (key) {
+          case "w":
+              var crash = new Audio('sounds/crash.mp3').play();
+              break;
+
+           case "a":
+              var kick = new Audio('sounds/kick-bass.mp3').play(); 
+              break;
+              
+           case "s":
+               var snare =new Audio('sounds/snare.mp3').play();
+               break;
+
+           case "d":
+               var tom1 = new Audio('sounds/tom-1.mp3').play();
+               break;
+
+           case "j":
+               var tom2 = new Audio('sounds/tom-2.mp3').play();
+               break; 
+           
+           case "k":
+               var tom3 = new Audio('sounds/tom-3.mp3').play();
+
+           case "l":
+               var tom4 = new Audio('sounds/tom-4.mp3').play();
+      
+          default: console.log(buttonInnerHTML)
+              break;
+      }
+  
+}
+
+ //Adding Animation 
+
+ function buttonAnimation(currentKey) {
+     var activeButton = document.querySelector("." + currentKey);
+       activeButton.classList.add("pressed");
+
+       setTimeout(function () {
+           activeButton.classList.remove("pressed");
+           
+       }, 100);
+ }
